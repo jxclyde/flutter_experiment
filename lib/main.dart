1040179e0614c9e4efe4a018ffdb208475b17761
Romko <roman.onushkiv@gmail.com>
2024-06-18 00:30:56 +0300

    Adding Scaffold wrapper; setting up colors and fonts for MainScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hey ninjas!'),
-
+  home: Scaffold( //it's like a wrapper to a few different layout widgets
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+      backgroundColor: Colors.indigo[400],
+    ),
+    body: Center(
+      child: Text(
+        'hello world!',
+        style: TextStyle(
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[700],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () { },
+      child: Text('click'),
+      backgroundColor: Colors.pink[700],
+    ),
+  ),
 ));
 
