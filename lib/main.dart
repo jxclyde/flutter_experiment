feed307869eab55fbbcfc4596abdcf01430d70f6
Romko <roman.onushkiv@gmail.com>
2024-06-22 19:43:59 +0300

    Applying row widget logic

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,22 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.indigo[400],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(130.0),
-        child: Text('hello'),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Hello world'),
+          OutlinedButton(onPressed: (){}, child: Text('click me'),
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.pink[700],
+          ),
+          ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text('inside container'),
+          ),
+        ],
       ),
       // Container(
       //   padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 40.0),
