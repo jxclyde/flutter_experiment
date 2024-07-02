6709e92d798252a6c5254503f2dee6b4bd4f5e66
Romko <roman.onushkiv@gmail.com>
2024-07-03 00:06:26 +0300

    New changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: QuoteList()
 ));
 
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
 
-class Home extends StatelessWidget {
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( //it's like a wrapper to a few different layout widgets
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('my first app'),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.indigo[400],
-      ),
-      body: Row(
-        children: [
-          Expanded(
-              child: Image.asset('assets/space1.jpg'),
-            flex: 3,
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.cyan,
-              child: Text('one'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.pinkAccent,
-              child: Text('two'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.amber,
-              child: Text('3'),
-            ),
-          )
-        ],
-      ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () { },
-        child: Text(
-          'click',
-          style: TextStyle(
-            fontSize: 18.0,
-          ),
-        ),
-        backgroundColor: Colors.pink[700],
       ),
     );
   }
 }
-
 
