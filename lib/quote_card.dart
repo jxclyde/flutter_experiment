bb0a3b96eff78dc3c1d8d540bb25c99bbd3aae8a
Romko <roman.onushkiv@gmail.com>
2024-07-06 22:57:04 +0300

    Adding logic to delete quotes with functions as parameters

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -3,7 +3,8 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote? quote;
-  QuoteCard({this.quote});
+  final Function? delete;
+  QuoteCard({this.quote, this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +32,12 @@ class QuoteCard extends StatelessWidget {
                   color: Colors.grey,
                 ),
               ),
+            SizedBox(height: 8.0),
+            ElevatedButton.icon(
+                onPressed: delete != null ? () => delete!() : null,
+                label: Text('Delete quote'),
+                icon: Icon(Icons.delete_forever),
+            ),
           ],
         ),
       ),
